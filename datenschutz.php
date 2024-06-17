4fd776b7c89e6b632fb68d385fb87204c36a7903
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-06-17 15:13:05 +0200

    update dsgvo.

--- datenschutz.php
+++ datenschutz.php
@@ -49,31 +49,6 @@
     </section>
   </main>
 
-  <footer class="px-4 mt-5">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-3">
-          <a href="index.html"><img src="images/bacio-logo.png" alt="Baio Logo" title="Bacio Logo"></a>
-        </div>
-        <div class="col-md-9 d-flex footer-content">
-          <div class="footer-menu d-flex">
-            <a href="impressum.html">Impressum</a>
-            <a href="datenschutz.html">Datenschutz</a>
-            <a href="kontakt.php">Kontakt</a>
-          </div>
-          <div class="footer-socials d-flex">
-            <a href="https://www.instagram.com/pizza.bacio/"><i class="fa-brands fa-instagram fs-1 fg-primary"></i></a>
-            <a href="https://www.facebook.com/bizza.bacio"><i class="fa-brands fa-facebook fs-1 fg-primary"></i></a>
-          </div>
-        </div>
-        <div class="col-md-12 d-flex justify-content-center">
-          <p>&copy; 2024 Pizza Bacio GmbH</p>
-          <!-- mit php Datum tauschen -->
-        </div>
-      </div>
-    </div>
-  </footer>
-
   <?php include 'footer.php'; ?>
 
   <script src="js/bootstrap.min.js"></script>
