4e6f11258f26db1b6274b6d328827e2798aa4fa9
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-06-20 16:14:27 +0200

    add aos.

--- head.php
+++ head.php
@@ -2,6 +2,7 @@
 <meta name="robots" content="noindex, nofollow">
 <meta name="description" content="">
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="fonts/fontawesome-6.5.2/css/all.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/animations.css">
--- index.php
+++ index.php
@@ -132,6 +132,7 @@
   <?php include 'footer.php'; ?>
 
   <script src="js/bootstrap.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
   <script src="js/script.js"></script>
   
 </body>
