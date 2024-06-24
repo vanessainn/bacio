9a5601b6d94f3d9810c74408454d6dccdf9b45e3
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-06-24 21:59:21 +0200

    add ueber uns.

---
+++ ueberuns.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <title>Unsere Pizzen | Bacio</title>
+  <?php include 'head.php'; ?>
+</head>
+<body>
+
+ <?php include 'nav.php'; ?>
+
+  <main>
+    <section id="history">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-12">
+            <h1>Unsere Geschichte</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+
+<?php include 'footer.php'; ?>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+  
+</body>
+</html>
