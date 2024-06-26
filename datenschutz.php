8517f5056edce35654f47953de67ddf344ed2028
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-06-26 23:24:08 +0200

    update impressum, datenschutz.

--- datenschutz.php
+++ datenschutz.php
@@ -16,33 +16,42 @@
           <div class="col-md-12">
             <h1>Datenschutzerklärung</h1>
             <p>Der Schutz Ihrer persönlichen Daten ist uns ein besonderes Anliegen. Wir verarbeiten Ihre Daten daher ausschließlich auf Grundlage der <a href="https://https://www.wko.at/datenschutz/eu-dsgvo-grundsaetze-verarbeitung" title="Datenschutzgrundverordnung" target="_blank">gesetzlichen Bestimmungen (DSGVO, TKG 2003)</a>. In diesen Datenschutzinformationen informieren wir Sie über die wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Website.</p>
-            <h2>Kontakt mit uns</h2>
-            <p>Wenn Sie per Formular auf der Website oder per E-Mail Kontakt mit uns aufnehmen, werden Ihre angegebenen Daten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen sechs Monate bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.</p>
-            <h2>Datenspeicherung</h2>
-            <p>Wir weisen darauf hin, dass zum Zweck des einfacheren Einkaufsvorganges und zur späteren Vertragsabwicklung vom Webshop-Betreiber im Rahmen von Cookies die IP-Daten des Anschlussinhabers gespeichert werden, ebenso wie Name, Anschrift und Kreditkartennummer […] des Käufers.</p>
-            <p>Die von Ihnen bereit gestellten Daten sind zur Vertragserfüllung bzw zur Durchführung vorvertraglicher Maßnahmen erforderlich. Ohne diese Daten können wir den Vertrag mit Ihnen nicht abschließen. Eine Datenübermittlung an Dritte erfolgt nicht, mit Ausnahme der Übermittlung der Kreditkartendaten an die abwickelnden Bankinstitute/Zahlungsdienstleister zum Zwecke der Abbuchung des Einkaufspreises, an das von uns beauftragte Transportunternehmen/Versandunternehmen zur Zustellung der Ware sowie an unseren Steuerberater zur Erfüllung unserer steuerrechtlichen Verpflichtungen.</p>
-            <p>Nach Abbruch des Einkaufsvorganges werden die bei uns gespeicherten Daten gelöscht. Im Falle eines Vertragsabschlusses werden sämtliche Daten aus dem Vertragsverhältnis bis zum Ablauf der steuerrechtlichen Aufbewahrungsfrist (7 Jahre) gespeichert.</p>
-            <p>Die Daten Name, Anschrift, gekaufte Waren und Kaufdatum werden darüber hinaus gehend bis zum Ablauf der Produkthaftung (10 Jahre) gespeichert.  Die Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen des § 96 Abs 3 TKG sowie des Art 6 Abs 1 lit a (Einwilligung) und/oder lit b (notwendig zur Vertragserfüllung) der DSGVO.</p>
-            <h2>Cookies</h2>
-            <p>Unsere Website verwendet so genannte Cookies. Dabei handelt es sich um kleine Textdateien, die mit Hilfe des Browsers auf Ihrem Endgerät abgelegt werden. Sie richten keinen Schaden an.</p>
-            <p>Wir nutzen Cookies dazu, unser Angebot nutzerfreundlich zu gestalten. Einige Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Sie ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.</p>
-            <p>Wenn Sie dies nicht wünschen, so können Sie Ihren Browser so einrichten, dass er Sie über das Setzen von Cookies informiert und Sie dies nur im Einzelfall erlauben.</p>
-            <p>Bei der Deaktivierung von Cookies kann die Funktionalität unserer Website eingeschränkt sein.</p>
-            <p>Die Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen des § 96 Abs 3 TKG sowie des Art 6 Abs 1 lit a (Einwilligung) und/oder f (berechtigtes Interesse) der DSGVO.</p>
-            <p>Unser Anliegen im Sinne der DSGVO (berechtigtes Interesse) ist die Verbesserung unseres Angebotes und unseres Webauftritts. Da uns die Privatsphäre unserer Nutzer wichtig ist, werden die Nutzerdaten pseudonymisiert.</p>
-            <h2>Newsletter</h2>
-            <p>Sie haben die Möglichkeit, über unsere Website unseren Newsletter zu abonnieren. Hierfür benötigen wir Ihre E-Mail-Adresse und ihre Erklärung, dass Sie mit dem Bezug des Newsletters einverstanden sind.</p>
-            <p>Sobald Sie sich für den Newsletter angemeldet haben, senden wir Ihnen ein Bestätigungs-E-Mail mit einem Link zur Bestätigung der Anmeldung.</p>
-            <p>Das Abo des Newsletters können Sie jederzeit stornieren. Senden Sie Ihre Stornierung bitte an folgende E-Mail-Adresse:<a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a>. Wir löschen anschließend umgehend Ihre Daten im Zusammenhang mit dem Newsletter-Versand.</p>
-            <h2>Ihre Rechte</h2>
-            <p>Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit, Widerruf und Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren. In Österreich ist dies die Datenschutzbehörde.</p>
-            <p>Sie erreichen uns unter folgenden Kontaktdaten:</p>
-            <p>Pizza Bacio GmbH<br>
-            Maximilianstraße 11<br>
-            6020 Innsbruck<br>
-            Österreich<br><br>
-            <a href="tel:+496542368512">T: +49 654 236 8512</a><br>
-            <a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a></p>
+            <h2 class="mt-5">Kontaktdaten des Verantwortlichen</h2>
+            <p>Sollten Sie Fragen zum Datenschutz oder zur Verarbeitung personenbezogener Daten haben, finden Sie nachfolgend die Kontaktdaten der verantwortlichen Person bzw. Stelle:<br>Pizza Bacio GmbH<br>Maximilianstraße 11<br>6020 Innsbruck<br>Österreich<br>Vertretungsberechtigt: Armando Ammirati<br>E-Mail: info@pizzabacio.at<br><a href="tel:+496542368512">T: +49 654 236 8512</a><br><a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a><br>
+            </p>
+            <h2 class="mt-5">Einleitung und Überblick</h2>
+            <p>Wir haben diese Datenschutzerklärung (Fassung 18.06.2024-122818254) verfasst, um Ihnen gemäß der Vorgaben der Datenschutz-Grundverordnung (EU) 2016/679 und anwendbaren nationalen Gesetzen zu erklären, welche personenbezogenen Daten (kurz Daten) wir als Verantwortliche – und die von uns beauftragten Auftragsverarbeiter (z. B. Provider) – verarbeiten, zukünftig verarbeiten werden und welche rechtmäßigen Möglichkeiten Sie haben. Die verwendeten Begriffe sind Kurz gesagt: Wir informieren Sie umfassend über Daten, die wir über Sie verarbeiten.</p>
+            <p>Datenschutzerklärungen klingen für gewöhnlich sehr technisch und verwenden juristische Fachbegriffe. Diese Datenschutzerklärung soll Ihnen hingegen die wichtigsten Dinge so einfach und transparent wie möglich beschreiben. Soweit es der Transparenz förderlich ist, werden technische Begriffe leserfreundlich erklärt, Links zu weiterführenden Informationen geboten und Grafiken zum Einsatz gebracht. Wir informieren damit in klarer und einfacher Sprache, dass wir im Rahmen unserer Geschäftstätigkeiten nur dann personenbezogene Daten verarbeiten, wenn eine entsprechende gesetzliche Grundlage gegeben ist. Das ist sicher nicht möglich, wenn man möglichst knappe, unklare und juristisch-technische Erklärungen abgibt, so wie sie im Internet oft Standard sind, wenn es um Datenschutz geht. Ich hoffe, Sie finden die folgenden Erläuterungen interessant und informativ und vielleicht ist die eine oder andere Information dabei, die Sie noch nicht kannten.</p>
+            <p>Wenn trotzdem Fragen bleiben, möchten wir Sie bitten, sich an die unten bzw. im Impressum genannte verantwortliche Stelle zu wenden, den vorhandenen Links zu folgen und sich weitere Informationen auf Drittseiten anzusehen. Unsere Kontaktdaten finden Sie selbstverständlich auch im Impressum.</p>
+            <h2 class="mt-5">Anwendungsbereich</h2>
+            <p>Diese Datenschutzerklärung gilt für alle von uns im Unternehmen verarbeiteten personenbezogenen Daten und für alle personenbezogenen Daten, die von uns beauftragte Firmen (Auftragsverarbeiter) verarbeiten. Mit personenbezogenen Daten meinen wir Informationen im Sinne des Art. 4 Nr. 1 DSGVO wie zum Beispiel Name, E-Mail-Adresse und postalische Anschrift einer Person. Die Verarbeitung personenbezogener Daten sorgt dafür, dass wir unsere Dienstleistungen und Produkte anbieten und abrechnen können, sei es online oder offline. Der Anwendungsbereich dieser Datenschutzerklärung umfasst:
+            <ul>
+              <li>alle Onlineauftritte (Websites, Onlineshops), die wir betreiben
+              </li>
+              <li>Social Media Auftritte und E-Mail-Kommunikation
+              </li>
+              <li>mobile Apps für Smartphones und andere Geräte
+              </li>
+            </ul>
+        Kurz gesagt: Die Datenschutzerklärung gilt für alle Bereiche, in denen personenbezogene Daten im Unternehmen über die genannten Kanäle strukturiert verarbeitet werden. Sollten wir außerhalb dieser Kanäle mit Ihnen inRechtsbeziehungen eintreten, werden wir Sie gegebenenfalls gesondert informieren.</p>
+            <h2 class="mt-5">Rechtsgrundlagen </h2>
+            <p>In der folgenden Datenschutzerklärung geben wir Ihnen transparente Informationen zu den rechtlichen Grundsätzen und Vorschriften, also den Rechtsgrundlagen der Datenschutz-Grundverordnung, die uns ermöglichen, personenbezogene Daten zu verarbeiten. <br> Was das EU-Recht betrifft, beziehen wir uns auf die VERORDNUNG (EU) 2016/679 DES EUROPÄISCHEN PARLAMENTS UND DES RATES vom 27. April 2016. Diese Datenschutz-Grundverordnung der EU können Sie selbstverständlich online auf EUR-Lex, dem Zugang zum EU-Recht, unter https://eur-lex.europa.eu/legal-content/DE/ALL/?uri=celex%3A32016R0679 nachlesen.</p>
+            <ul>Wir verarbeiten Ihre Daten nur, wenn mindestens eine der folgenden Bedingungen zutrifft: <br>
+              <li>1. Einwilligung (Artikel 6 Absatz 1 lit. a DSGVO): Sie haben uns Ihre Einwilligung gegeben, Daten zu einem bestimmten Zweck zu verarbeiten. Ein Beispiel wäre die Speicherung Ihrer eingegebenen Daten eines Kontaktformulars.</li>
+              <li>2. Vertrag (Artikel 6 Absatz 1 lit. b DSGVO): Um einen Vertrag oder vorvertragliche Verpflichtungen mit Ihnen zu erfüllen, verarbeiten wir Ihre Daten. Wenn wir zum Beispiel einen Kaufvertrag mit Ihnen abschließen, benötigen wir vorab personenbezogene Informationen.</li>
+              <li>3. Rechtliche Verpflichtung (Artikel 6 Absatz 1 lit. c DSGVO): Wenn wir einer rechtlichen Verpflichtung             Buchhaltung aufzuheben. Diese enthalten in der Regel personenbezogene Daten.</li>
+              <li>4. Berechtigte Interessen (Artikel 6 Absatz 1 lit. f DSGVO): Im Falle berechtigter Interessen, die Ihre Grundrechte nicht einschränken, behalten wir uns die Verarbeitung personenbezogener Daten vor. Wir müssen zum Beispiel gewisse Daten verarbeiten, um unsere Website sicher und wirtschaftlich effizient betreiben zu können. Diese Verarbeitung ist somit ein berechtigtes Interesse.</li>
+            </ul>
+            <p>Weitere Bedingungen wie die Wahrnehmung von Aufnahmen im öffentlichen Interesse und Ausübung öffentlicher Gewalt sowie dem Schutz lebenswichtiger Interessen treten bei uns in der Regel nicht auf. Soweit eine solche Rechtsgrundlage doch einschlägig sein sollte, wird diese an der entsprechenden Stelle ausgewiesen.</p>
+            <p>Zusätzlich zu der EU-Verordnung gelten auch noch nationale Gesetze:</p>
+            <ul>
+              <li>In Österreich ist dies das Bundesgesetz zum Schutz natürlicher Personen bei der Verarbeitung personenbezogener Daten (Datenschutzgesetz), kurz DSG.</li>
+              <li>
+                In Deutschland gilt das Bundesdatenschutzgesetz, kurz BDSG.</li>
+            </ul>
+            <p>Sofern weitere regionale oder nationale Gesetze zur Anwendung kommen, informieren wir Sie in den folgenden Abschnitten darüber.</p>
+            <h2 class="mt-5">Speicherdauer </h2>
+            <p>Dass wir personenbezogene Daten nur so lange speichern, wie es für die Bereitstellung unserer Dienstleistungen und Produkte unbedingt notwendig ist, gilt als generelles Kriterium bei uns. Das bedeutet, dass wir personenbezogene Daten löschen, sobald der Grund für die Datenverarbeitung nicht mehr vorhanden ist. In einigen Fällen sind wir gesetzlich dazu verpflichtet, bestimmte Daten auch nach Wegfall des ursprüngliches Zwecks zu speichern, zum Beispiel zu Zwecken der Buchführung. <br>Sollten Sie die Löschung Ihrer Daten wünschen oder die Einwilligung zur Datenverarbeitung widerrufen, werden die Daten so rasch wie möglich und soweit keine Pflicht zur Speicherung besteht, gelöscht. <br>Über die konkrete Dauer der jeweiligen Datenverarbeitung informieren wir Sie weiter unten, sofern wir weitere Informationen dazu haben.</p>
           </div>
         </div>
       </article>
--- impressum.php
+++ impressum.php
@@ -15,18 +15,21 @@
         <div class="row">
           <div class="col-md-12">
             <h1>Impressum</h1>
-            <p>Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz.</p><br>
-            <p>Pizza Bacio GmbH<br>Armando Ammirati & Andreas Schmid<br>Maximilianstraße 11<br>6020 Innsbruck<br>Österreich</p><br>
-            <p>UID-Nummer: ATU8737472<br><a href="tel:+496542368512">T: +49 654 236 8512</a><br><a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a></p><br>
-            <p>Aufsichtsbehörde/Gewerbebehörde: Bezirkshauptmannschaft Innsbruck</p>
-            <h2 class="mt-5">Urheberrechtshinweis</h2>
-            <p>Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen. Sollten Sie auf dieser Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir Sie uns zu kontaktieren.<br><a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a></p>
-            <h2 class="mt-5">Bildnachweis</h2>
-            <p>Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.
-              Die Bilderrechte liegen bei den folgenden Fotografen und Unternehmen:<br>Pizza Bacio GmbH<br><a href="www.unsplash.com">www.unsplash.com</a>
-              </p>
-          </div>
-        </div>
+            <p>Pizza Bacio<br>Armando Ammirati & Andreas Schmid<br>Maximilianstraße 11<br>6020 Innsbruck<br>Österreich</p><br>
+            <p><a href="tel:+496542368512">T: +49 654 236 8512</a><br><a href="mailto:info@pizzabacio.at">M: info@pizzabacio.at</a></p><br>
+            <p>Firmenbuchnummer: 123456a<br>Firmenbuchgericht: Innsbruck Gerichtstand <br>Firmensitz: Maximilianstraße 11 <br> Unternehmensgegenstand: Pizzeria, Lebensmittel<br>UID-Nummer: ATU8737472<br><br>Mitglied bei: WKO, Landesinnung <br></p>
+            <p>Aufsichtsbehörde/Gewerbebehörde: Bezirkshauptmannschaft Innsbruck <br>Webseite: https://www.aufsichtsbeoerde-musterhausen.at/ <br>Anschrift <br>Haus A <br>Landesmuseum Straße, 6020 Innsbruck <br>Österreich <br>Berufsbezeichnung: Gastgewerbe <br>Verleihungsstaat: Österreich <br>Datenschutz Vertretungsberechtigter: Armando Ammirati <br>E-Mail der Datenschutzstelle: info@pizzabacio.at <br>Tel.: +49 654 236 8512 <br>Kontakt: <br>Bacio GmbH <br>Maximilanstraße 11 <br>6020 Innsbruck <br>Österreich</p>
+            <h2 class="mt-5">Haftung für Inhalte dieser Website</h2>
+        <p>Wir entwickeln die Inhalte dieser Website ständig weiter und bemühen uns korrekte und aktuelle Informationen bereitzustellen. Leider können wir keine Haftung für die Korrektheit aller Inhalte auf dieser Website übernehmen, speziell für jene, die seitens Dritter bereitgestellt wurden. Als Diensteanbieter sind wir nicht verpflichtet, die von Ihnen übermittelten oder gespeicherten Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.<br>Unsere Verpflichtungen zur Entfernung von Informationen oder zur Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen aufgrund von gerichtlichen oder behördlichen Anordnungen bleiben auch im Falle unserer Nichtverantwortlichkeit davon unberührt.<br>Sollten Ihnen problematische oder rechtswidrige Inhalte auffallen, bitte wir Sie uns umgehend zu kontaktieren, damit wir die rechtswidrigen Inhalte entfernen können. Sie finden die Kontaktdaten im Impressum.</p>
+        <h2 class="mt-5">Haftung für Links auf dieser Website</h2>
+        <p>Unsere Website enthält Links zu anderen Websites für deren Inhalt wir nicht verantwortlich sind. Haftung für verlinkte Websites besteht für uns nicht, da wir keine Kenntnis rechtswidriger Tätigkeiten hatten und haben, uns solche Rechtswidrigkeiten auch bisher nicht aufgefallen sind und wir Links sofort entfernen würden, wenn uns Rechtswidrigkeiten bekannt werden.<br>Wenn Ihnen rechtswidrige Links auf unserer Website auffallen, bitte wir Sie uns zu kontaktieren. Sie finden die Kontaktdaten im Impressum.</p>
+        <h2 class="mt-5">Urheberrechtshinweis</h2>
+        <p>Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen dem Urheberrecht. Bitte fragen Sie uns bevor Sie die Inhalte dieser Website verbreiten, vervielfältigen oder verwerten wie zum Beispiel auf anderen Websites erneut veröffentlichen. Falls notwendig, werden wir die unerlaubte Nutzung von Teilen der Inhalte unserer Seite rechtlich verfolgen.<br>Sollten Sie auf dieser Webseite Inhalte finden, die das Urheberrecht verletzen, bitten wir Sie uns zu kontaktieren.</p>
+        <h2 class="mt-5">Bildernachweis</h2>
+        <p>Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich geschützt.<br>
+        Alle Texte sind urheberrechtlich geschützt. <br></p>
+
+        <p>Impressum wurde erstellt mit dem<a class="text-decoration-none color-black" href="https://www.adsimple.at/impressum-generator/" title="Impressum Generator Österreich von AdSimple"> Impressum Generator </a> von AdSimple</p>
       </div>
     </section>
   </main>
