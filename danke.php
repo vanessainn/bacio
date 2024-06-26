a8245eeaddcb01bb8b784bed65ed4002351ab97f
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-06-26 21:46:21 +0200

    update danke php.

--- danke.php
+++ danke.php
@@ -11,16 +11,17 @@
 
   <section id="danke">
     <div class="container">
-      <div class="row">
-        <div class="col-md-12">
+      <div class="row d-flex align-items-center text-center">
+        <div class="col-md-12 grazie">
           <h1>Danke für deine Nachricht !</h1>
-          <p>Du erhälst in kürze eine Mailbestätigung.</p>
-        </div>
+          <p>Du erhälst in kürze eine Mail zur Bestätigung deiner Nachricht.</p>
+          <img src="medien/bacio-pizza-animation.gif" alt="Eine Pizza wird gezeichnet" title="Eine Pizza wird gezeichnet" class="img-fluid">
+        </div>>
       </div>
     </div>
   </section>
 
-  <footer class="px-4 mt-5 fixed-bottom">
+  <footer class="px-4 mt-5 fixed-bottom pt-5">
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-3">
@@ -33,8 +34,8 @@
             <a href="kontakt.php">Kontakt</a>
           </div>
           <div class="footer-socials d-flex">
-            <a href="https://www.instagram.com/pizza.bacio/"><i class="fa-brands fa-instagram fs-1 fg-primary"></i></a>
-            <a href="https://www.facebook.com/bizza.bacio"><i class="fa-brands fa-facebook fs-1 fg-primary"></i></a>
+            <a href="https://www.instagram.com/pizza.bacio/"><i class="fa-brands fa-instagram fs-1 fg-white"></i></a>
+            <a href="https://www.facebook.com/bizza.bacio"><i class="fa-brands fa-facebook fs-1 fg-white"></i></a>
           </div>
         </div>
         <div class="col-md-12 d-flex justify-content-center">
